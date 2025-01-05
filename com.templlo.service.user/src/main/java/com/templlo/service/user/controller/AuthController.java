b3367ddda9f31b9828b0448969f03c7e57aead7e
jaehee-An <jhh9615@gmail.com>
2025-01-06 03:19:45 +0900

    ci workflow 테스트

--- com.templlo.service.user/src/main/java/com/templlo/service/user/controller/AuthController.java
+++ com.templlo.service.user/src/main/java/com/templlo/service/user/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.templlo.service.user.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.templlo.service.user.common.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -9,4 +12,9 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+	@PostMapping("/login")
+	public ApiResponse<Void> login() {
+		return ApiResponse.success();
+	}
 }
