460fe92a3363b4afe3d6d1d9f39a2a7efe8b464f
jaehee-An <jhh9615@gmail.com>
2025-01-06 03:07:24 +0900

    [feat] AuthController 클래스 작성

---
+++ com.templlo.service.user/src/main/java/com/templlo/service/user/controller/AuthController.java
@@ -0,0 +1,12 @@
+package com.templlo.service.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+}
