a1d35eea476e039150b87721f750b45bd6d4e73e
jaehee-An <jhh9615@gmail.com>
2025-01-06 14:55:47 +0900

    [feat] 로그인 기능 구현

--- com.templlo.service.user/src/main/java/com/templlo/service/user/controller/AuthController.java
+++ com.templlo.service.user/src/main/java/com/templlo/service/user/controller/AuthController.java
@@ -1,11 +1,16 @@
 package com.templlo.service.user.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.templlo.service.user.common.ApiResponse;
+import com.templlo.service.user.dto.LoginRequestDto;
+import com.templlo.service.user.dto.TokenDto;
+import com.templlo.service.user.service.AuthService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -13,8 +18,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthController {
 
+	private final AuthService authService;
+
 	@PostMapping("/login")
-	public ApiResponse<Void> login() {
-		return ApiResponse.success();
+	public ApiResponse<TokenDto> login(@Valid @RequestBody LoginRequestDto request) {
+		TokenDto tokenDto = authService.login(request);
+		return ApiResponse.success(tokenDto);
 	}
 }
