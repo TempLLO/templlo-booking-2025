7d51f87ffc8e79253d8b27a718d9adc37b8dbc38
jaehee-An <jhh9615@gmail.com>
2025-01-09 17:35:01 +0900

    :sparkles: 이벤트 기반 유저 리뷰횟수 업데이트 consume 서비스 개발

---
+++ com.templlo.service.user/src/main/java/com/templlo/service/user/external/kafka/consumer/EventListener.java
@@ -0,0 +1,34 @@
+package com.templlo.service.user.external.kafka.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.templlo.service.user.external.kafka.consumer.dto.ReviewCreatedEventDto;
+import com.templlo.service.user.service.UpdateUserService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = " Event Listener ")
+@Component
+@RequiredArgsConstructor
+public class EventListener {
+
+	private final ObjectMapper objectMapper;
+	private final UpdateUserService updateUserService;
+
+	@KafkaListener(topics = "review-created", groupId = "user-consumer-group-review", containerFactory = "listenerFactory")
+	public void handleReviewCreated(String message) {
+		log.info("Consumed Message From Topic : {} ", "review-created");
+		try {
+			// Todo serializer class 따로 구현할지
+			ReviewCreatedEventDto eventDto = objectMapper.readValue(message, ReviewCreatedEventDto.class);
+			updateUserService.updateReviewCount(eventDto);
+		} catch (JsonProcessingException e) {
+			log.error("JSON Parsing Error : {}", e.getMessage());
+		}
+	}
+
+}
